e511dfc48fb8e4aa05935d7b5215bd03fc30921d
asadulislam <asadulislam0288@gmail.com>
2025-08-23 14:36:45 +0600

    added by mobile bank

--- js/payoo.js
+++ js/payoo.js
@@ -3,6 +3,19 @@ document
   .getElementById("loginButton")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    console.log("login Button Click");
-    console.log(event);
+    const mobileNumber = 1327822021;
+    const pinNuber = 1234;
+    const mobileNumbervalue = document.getElementById("mobile-number").value;
+    const mobileNumberValueConvert = parseInt(mobileNumbervalue);
+    const pinNumberValue = document.getElementById("pin-number").value;
+    const pinNumberValueConvert = parseInt(pinNumberValue);
+
+    if (
+      mobileNumberValueConvert === mobileNumber &&
+      pinNumberValueConvert === pinNuber
+    ) {
+      window.location.href = "./home.html";
+    } else {
+      alert("Invaild Credentital");
+    }
   });
