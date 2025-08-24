9ecb5b8ed68e258e95edacdda21d9cb05bd764ea
asadulislam <asadulislam0288@gmail.com>
2025-08-24 10:23:39 +0600

    added by mobile bank4

--- home.js
+++ home.js
@@ -28,3 +28,57 @@ document
     document.getElementById("available-balance").innerText =
       totalAvailabaleBalance;
   });
+
+// cash out feture
+
+// button
+document
+  .getElementById("withdraw-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    // ###########
+    const amount = parseInt(document.getElementById("withdraw-amount").value);
+
+    const accountNumber = document.getElementById("agent-number").value;
+
+    const pin = parseInt(document.getElementById("amount-pin").value);
+
+    const availableBlance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+    // console.log(amount, availableBlance);
+    const totalNewAvailabaleBalance = availableBlance - amount;
+    // console.log(totalNewAvailabaleBalance);
+
+    if (accountNumber.length < 11) {
+      alert("Please provaid valid account number");
+      return;
+    }
+
+    if (pin !== validPin) {
+      alert("Please Provaid a Valid Pin Number");
+      return;
+    }
+
+    // balance add
+
+    document.getElementById("available-balance").innerText =
+      totalNewAvailabaleBalance;
+  });
+
+// toggeling feture
+
+document
+  .getElementById("add-money-button")
+  .addEventListener("click", function () {
+    document.getElementById("cash-out-paraents").style.display = "none";
+    document.getElementById("add-money-parents").style.display = "block";
+  });
+document
+  .getElementById("cash-out-button")
+  .addEventListener("click", function () {
+    document.getElementById("add-money-parents").style.display = "none";
+    document.getElementById("cash-out-paraents").style.display = "block";
+  });
+
+//
